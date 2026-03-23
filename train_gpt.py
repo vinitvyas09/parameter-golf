397a400352d81c37a340b6847a904aac6ace89c4
Vinit Vyas <vinitvyas09@gmail.com>
2026-03-23 09:42:25 +0000

    E-009: warmdown_iters 200->400, swa_every 50->25

--- train_gpt.py
+++ train_gpt.py
@@ -52,7 +52,7 @@ class Hyperparameters:
 
     # Training length.
     iterations = int(os.environ.get("ITERATIONS", 20000))
-    warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 200))
+    warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 400))
     warmup_steps = int(os.environ.get("WARMUP_STEPS", 20))
     train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 524_288))
     train_seq_len = int(os.environ.get("TRAIN_SEQ_LEN", 1024))
@@ -93,7 +93,7 @@ class Hyperparameters:
 
     # SWA (stochastic weight averaging).
     swa_start_frac = float(os.environ.get("SWA_START_FRAC", 0.5))
-    swa_every = int(os.environ.get("SWA_EVERY", 50))
+    swa_every = int(os.environ.get("SWA_EVERY", 25))
 
 # -----------------------------
 # MUON OPTIMIZER 
